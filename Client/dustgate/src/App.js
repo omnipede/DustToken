3cf5e4b7faa3557fef10c699b927b5c862b7a4d9
omnipede <omnipede@naver.com>
2019-05-08 14:46:45 +0900

    Layout 추가
    Simple left-right Layout 설정.

--- Client/dustgate/src/App.js
+++ Client/dustgate/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import {
-  Layout, Menu, Breadcrumb, Icon
+  Layout, Menu, Breadcrumb, Icon, DatePicker, message
 } from 'antd'; 
-import './App.css';
+import "antd/dist/antd.css";
+import './index.css';
+//import './App.css';
 
 const {
   Header, Content, Footer, Sider,
@@ -11,35 +13,72 @@ const {
 const SubMenu = Menu.SubMenu;
 
 class App extends React.Component {
-
   state = {
-    collapsed: false,
+    collapsed: false
   };
 
   onCollapse = (collapsed) => {
-    console.log (collapsed);
     this.setState({collapsed});
-  }
-  
+  };
+
+  handleChange = date => {
+    message.info(`Selected Date: ${date ? date.format("YYYY-MM-DD") : "None"}`);
+    this.setState({ date });
+  };
+
   render() {
+    const { date } = this.state;
     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world !!! <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-    )
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="user" /><span>User</span></span>}
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="team" /><span>Team</span></span>}
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '16px 16px' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    );
   }
 }
 
