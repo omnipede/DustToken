bfc9d24ebe428cdc0248671dba8cc7d8313ad6c9
omnipede <omnipede@naver.com>
2019-05-08 14:58:53 +0900

    Menu item 추가
    Login, Device, API, Block chain 메뉴 추가

--- Client/dustgate/src/App.js
+++ Client/dustgate/src/App.js
@@ -30,6 +30,7 @@ class App extends React.Component {
     const { date } = this.state;
     return (
       <Layout style={{ minHeight: '100vh' }}>
+        
         <Sider
           collapsible
           collapsed={this.state.collapsed}
@@ -39,33 +40,27 @@ class App extends React.Component {
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1">
               <Icon type="pie-chart" />
-              <span>Option 1</span>
+              <span> Intro. </span>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="desktop" />
-              <span>Option 2</span>
+              <span> login / signin </span>
             </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="user" /><span>User</span></span>}
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={<span><Icon type="team" /><span>Team</span></span>}
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file" />
-              <span>File</span>
+            <Menu.Item key="3">
+              <Icon type="desktop" />
+              <span> Device </span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="desktop" />
+              <span> API </span>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <Icon type="desktop" />
+              <span> Block chain </span>
             </Menu.Item>
           </Menu>
         </Sider>
+
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }} />
           <Content style={{ margin: '16px 16px' }}>
