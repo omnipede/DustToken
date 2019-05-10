0edb0974415e523faf0066ad988842ec90d42eae
unknown <omnipede@naver.com>
2019-05-10 12:38:05 +0900

    Server template 작성

---
+++ Server/server/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.listen(3001, function(){
+    console.log('Server is listening on port 3000!');
+});
