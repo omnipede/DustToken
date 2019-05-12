8073ba9bb2c297b76f06060c9b56fe7ac75420b0
omnipede <omnipede@naver.com>
2019-05-12 17:12:18 +0900

    Signin.js 수정
    Field decorator 및 submit 기능 추가.

--- Client/dustgate/src/Components/Signin.js
+++ Client/dustgate/src/Components/Signin.js
@@ -9,20 +9,80 @@ import './style/style.css';
 const SigninForm = Form.create({name: 'signin'})(
 
     class extends React.Component {
+
+        state = {
+            confirmDirty: false
+        }
+
+        handleConfirmBlur = (e) => {
+            const value = e.target.value;
+            this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+        }
+
+        compareToFirstPassword = (rule, value, callback) => {
+            const form = this.props.form;
+            if (value && value !== form.getFieldValue('password')) {
+                callback('Two passwords that you enter is inconsistent!');
+            } else {
+                callback();
+            }
+        }
+
+        validateToNextPassword = (rule, value, callback) => {
+            const form = this.props.form;
+            if (value && this.state.confirmDirty) {
+                form.validateFields(['confirm'], {force: true});
+            }
+            callback();
+        }
+
         render(){
             const {getFieldDecorator} = this.props.form;
             return(
                 <div>
-                    <Modal visible={this.props.visible} footer={null}>
-                        <Form>
+                    <Modal visible={this.props.visible} footer={null} onCancel = {this.props.onCancel} >
+                        <Form onSubmit = {this.props.onSubmit}> {/*
                             <Form.Item label="E-mail">
-                                <Input />
+                                {getFieldDecorator('email', {
+                                    rules: [{
+                                        type: 'email', message: 'The input is not valid E-mail', 
+                                    }, {
+                                        required: 'true', message: 'Please input your E-mail!',
+                                    }], 
+                                }) (
+                                    <Input />
+                                )}
+                                </Form.Item> */}
+                            <Form.Item label="Username">
+                                {getFieldDecorator('username', {
+                                    rules: [{
+                                        required: true, message: 'Please input your username!',
+                                    }]
+                                }) (
+                                    <Input />
+                                )}
                             </Form.Item>
                             <Form.Item label="Password" >
-                                <Input type="password" />
+                                {getFieldDecorator('password', {
+                                    rules: [{
+                                        required: true, message: 'Please input your password!'
+                                    }, {
+                                        validator: this.validateToNextPassword,
+                                    }]
+                                })(
+                                    <Input type="password" />
+                                )}
                             </Form.Item>
                             <Form.Item label="Confirm Password">
-                                <Input type="password" />
+                                {getFieldDecorator('confirm', {
+                                    rules: [{
+                                        required: true, message: 'Please confirm your password!'
+                                    }, {
+                                        validator: this.compareToFirstPassword,
+                                    }]
+                                })(
+                                    <Input type="password" onBlur={this.handleConfirmBlur} />
+                                )}
                             </Form.Item>
                             <Form.Item>
                                 <Button type="primary" htmlType="submit">Sign in</Button>
@@ -46,6 +106,22 @@ class SigninPage extends React.Component {
         })
     }
 
+    handleCancel = (e) => {
+        this.setState({
+            visible: false
+        })
+    } 
+
+    handleSubmit = (e) => {
+        const form = this.formRef.props.form;
+        e.preventDefault();
+        form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        })
+    }
+
     saveFormRef = (formRef) => {
         this.formRef = formRef;
     }
@@ -57,6 +133,12 @@ class SigninPage extends React.Component {
                     className="signin_Btn" onClick={this.showModal}
                 > Sign in
                 </Button>
+
+                <SigninForm 
+                    wrappedComponentRef = {this.saveFormRef}
+                    visible = {this.state.visible}
+                    onCancel = {this.handleCancel} 
+                    onSubmit = {this.handleSubmit} />
             </React.Fragment>
         )
     }
