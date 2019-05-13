5a0dada8f7b13b654940fe8a68f62cdb1567ed37
omnipede <omnipede@naver.com>
2019-05-13 15:48:18 +0900

    Menubar 컴포넌트 추가
    App에서 렌더링하던 메뉴바를 독립된 컴포넌트로 옮김.

--- Client/dustgate/src/App.js
+++ Client/dustgate/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {
-  Layout, Menu, Icon, Button
+  Layout, Icon, Button
 } from 'antd'; 
 
 import LoginPage from './Components/Login';
+import Menubar from './Components/Menubar';
 import SigninPage from './Components/Signup';
 
 import "antd/dist/antd.css";
 import './App.css';
 
-import logo from './logo_white.png';
 import logo_black from './logo_black.png';
 
 const {
-  Header, Content, Footer, Sider
+  Header, Content, Footer,
 } = Layout;
 
 class App extends React.Component {
   state = {
     collapsed: false, 
-    menuKey: "Intro.",
+    nav: "Intro.",
     visible: false,
     login: false
   };
@@ -27,11 +27,6 @@ class App extends React.Component {
   userInfo = {
     username: null
   }
-
-  /* Menu collapse callback */
-  onCollapse = (collapsed) => {
-    this.setState({collapsed});
-  };
 
   /* Menu toggle callback. */
   toggle = () => {
@@ -41,11 +36,11 @@ class App extends React.Component {
   }
 
   /* Menu handling callback */
-  onMenuClick = (e) => {
+  handleMenuClick = (e) => {
     /* Debug purpose */
     console.log('click', e);
     this.setState({
-      menuKey: e.key,
+      nav: e.key,
     })
   };
 
@@ -57,55 +52,32 @@ class App extends React.Component {
     })
   }
 
-  handleLogout = (username) => {
+  handleLogout = (e) => {
     this.userInfo.username = undefined;
     this.setState({
       login: false
     })
   }
 
+  getContent = () => {
+    switch(this.state.nav){
+      case "Intro.":
+      break;
+      case "Device":
+      break;
+      case "API":
+      break;
+      case "Block chain":
+      break;
+      default:
+      break;
+    }
+  }
+
   render() {
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={this.state.collapsed}
-        >
-          <div>
-          {
-            this.state.collapsed === false
-              ? (
-              <div className="logo"> 
-              <img src={logo} width="168px" height="32px" alt="Logo" />
-              </div>
-              )
-              : (
-              <div className="logo"> 
-              </div>
-              )
-          }
-          </div>
-          <Menu theme="dark" defaultSelectedKeys={['Intro.']} mode="inline" onClick = {this.onMenuClick}>
-            <Menu.Item key="Intro.">
-              <Icon type="pie-chart" />
-              <span> Intro. </span>
-            </Menu.Item>
-            <Menu.Item key="Device">
-              <Icon type="desktop" />
-              <span> Device </span>
-            </Menu.Item>
-            <Menu.Item key="API">
-              <Icon type="desktop" />
-              <span> API </span>
-            </Menu.Item>
-            <Menu.Item key="Block chain">
-              <Icon type="desktop" />
-              <span> Block chain </span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-
+        <Menubar collapsed={this.state.collapsed} onMenuClick={this.handleMenuClick} />
         <Layout>
           <Header style={{ background: 'rgba(0, 0, 0, 0)', padding: 0 }} >
             <Icon className="trigger" type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toggle} style={{fontSize: '32px'}}/>
@@ -117,14 +89,13 @@ class App extends React.Component {
               <SigninPage /> 
               </div>
               : <div style={{width: '256px', height: '64px', float: 'right'}}>
-              Welcome <i>{"" + this.userInfo.username}!</i> 
+              Welcome <i>{"" + this.userInfo.username}! {' '} </i> 
               <Button type="danger" onClick={this.handleLogout}>log out</Button>
               </div>}
-              
           </Header>
           <Content style={{ margin: '16px 16px' }}>
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-            { this.state.menuKey === "login" 
+            { this.state.nav === "login" 
             /*getContent() 구현 */
             ? <div> this is page two </div>
             : <div> this is not page two </div>
@@ -137,7 +108,6 @@ class App extends React.Component {
           </Footer>
         </Layout>
       </Layout>
-
     );
   }
 }
---
+++ Client/dustgate/src/Components/Menubar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+    Layout, Menu, Icon,
+} from 'antd';
+
+import "antd/dist/antd.css";
+import '../App.css';
+
+import logo from '../logo_white.png';
+const {
+    Sider
+} = Layout;
+
+class Menubar extends React.Component {
+   render() {
+       return(
+           <Sider
+               trigger={null}
+               collapsible
+               collapsed={this.props.collapsed}
+           >
+               <div>
+                   {
+                       this.props.collapsed === false
+                           ? (
+                               <div className="logo">
+                                   <img src={logo} width="168px" height="32px" alt="Logo" />
+                               </div>
+                           )
+                           : (
+                               <div className="logo">
+                               </div>
+                           )
+                   }
+               </div>
+               <Menu theme="dark" defaultSelectedKeys={['Intro.']} mode="inline" onClick={this.props.onMenuClick}>
+                   <Menu.Item key="Intro.">
+                       <Icon type="pie-chart" />
+                       <span> Intro. </span>
+                   </Menu.Item>
+                   <Menu.Item key="Device">
+                       <Icon type="desktop" />
+                       <span> Device </span>
+                   </Menu.Item>
+                   <Menu.Item key="API">
+                       <Icon type="desktop" />
+                       <span> API </span>
+                   </Menu.Item>
+                   <Menu.Item key="Block chain">
+                       <Icon type="desktop" />
+                       <span> Block chain </span>
+                   </Menu.Item>
+               </Menu>
+           </Sider>
+       )
+   }
+}
+
+export default Menubar;
