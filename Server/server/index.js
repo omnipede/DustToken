d9e9582d6af1edc73eb6728110d230249ef84fc0
omnipede <omnipede@naver.com>
2019-05-17 13:04:48 +0000

    Install Web3.js

--- Server/server/index.js
+++ Server/server/index.js
@@ -1,5 +1,18 @@
+const config = require( '../../Ethereum/config.json');
+const Web3 = require ('web3');
+
 const express = require('express');
 const app = express();
+
+let web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io'));
+app.get('/', function(req, res) {
+  res.send("Hello world");
+})	
+
+web3.eth.getBalance('0x0164214FF43A46c8ad6C399811576ABFaB68FA42', (err, data) => {
+  console.log(err);
+  console.log(data);
+})
 
 app.get('/data', function(req, res){
   var r = {};
