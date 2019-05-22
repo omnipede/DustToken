85eecb09e872d726e28c4e9545c4a9cd9d063c6d
omnipede <omnipede@naver.com>
2019-05-23 00:27:51 +0900

    Device 컴포넌트 서버 연동 구현
    axios 모듈 이용해서 서버에 저장된 디바이스 목록 가져오고
    프론트엔드 상에서 수정 가능하도록 구현함.

--- Client/dustgate/src/Components/Device.js
+++ Client/dustgate/src/Components/Device.js
@@ -1,107 +1,144 @@
 import React from 'react';
 import {Button, Icon, Table, Modal, Layout, Form, Input} from 'antd';
 import 'antd/dist/antd.css';
+import axios from 'axios';
 const {
     Header
 } = Layout;
-const columns = [
-    {
-        title: 'Device id',
-        dataIndex: 'device_id',
-        key: 'device_id'
-    },
-    {
-        title: 'Wallet',
-        dataIndex: 'wallet',
-        key: 'wallet',
-    },
-    {
-        title: 'Action',
-        dataIndex: 'action',
-        key: 'action'
-    }
-]
-
-let data = [
-    {
-        key: '1',
-        device_id: 'John Brown',
-        wallet: 32
-    },
-    {
-        key: '2',
-        device_id: 'Jim Green',
-        wallet: 42
-    }
-]
 
 const Device = Form.create({name: 'device_register'})( 
     class extends React.Component{
-    state = {
-        visible: false,
-        key: '1'
-    }
-    add = (e) => {
-        this.setState({visible: true});
-    }
-    cancel = () => {
-        this.setState({visible: false});
-    }
+        state = {
+            visible: false,
+            dataSource: [],
+            dataCount: 0
+        }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const {validateFields} = this.props.form;
-        validateFields((err, values) => {
-            if (!err){
-                console.log('Received values of form: ', values);
-                this.props.form.resetFields();
-                this.setState({visible: false});
-                data.push({key: this.state.key, device_id: values.device_id, wallet: values.wallet_address})
+        columns = [
+            { title: 'Device id', dataIndex: 'device_id', key: 'device_id'},
+            { title: 'Location', dataIndex: 'location', key: 'location'},
+            { title: 'Wallet Address', dataIndex: 'wallet_address', key: 'wallet_address', },
+            { title: 'Action', dataIndex: 'action',
+                render: (text, record) => <Button type="danger" onClick={() => this.handleDelete(record.key)}> Delete </Button>
             }
-        })
-    }
+        ]
+        
+        componentDidMount() {
+            axios.get('http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/device/list?username=admin')
+            .then(response => {
+                let t = [];
+                let c = this.state.dataCount;
+                response.data.forEach(function(item, index, array) {
+                    const newData = {
+                        key: c,
+                        location: item.location,
+                        device_id: item.device_id,
+                        wallet_address: item.wallet_address
+                    }
+                    t.push(newData);
+                    c += 1;
+                })
+                this.setState({
+                    dataCount: c,
+                    dataSource: [...t]
+                })
+            })
+        }
 
-    getRegister (){
-        const {getFieldDecorator} = this.props.form;
-        return (
-        <Modal visible = {this.state.visible} footer = {null} onCancel={this.cancel}>
-            <Layout style={{ background: '#fff' }}>
-                <Header style={{background: '#fff', padding: 0}} align="center">
-                    <h1>Register device</h1>
-                </Header>
-                <Form>
-                    <Form.Item label="Device id">
-                        {getFieldDecorator('device_id', {
-                            rules: [{required: true, message: 'Please input device id!'},]
-                        })(
-                            <Input />
-                        )}
-                    </Form.Item>
-                    <Form.Item label="Wallet address">
-                        {getFieldDecorator('wallet_address', {
-                            rules: [{required: true, message: 'Please input your wallet address!'},]
-                        })(
-                            <Input />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" onClick={this.handleSubmit} htmlType="submit">Register</Button>
-                    </Form.Item>
-                </Form>
-            </Layout>
-        </Modal>
-        ) 
-    }
-    render() {
-        return(
-            <div style={{padding: '0 0 0 5%'}}>
-                {this.getRegister()}
-                <div style={{padding: '0 0 0 65%'}}>
-                    <Button type="primary" onClick={this.add}> <Icon type="plus" /> Add </Button>
+        add = (e) => {
+            this.setState({ visible: true });
+        }
+        cancel = () => {
+            this.setState({ visible: false });
+        }
+
+        handleDelete = (key) => {
+            const dataSource = [...this.state.dataSource];
+            this.setState({
+                dataSource: dataSource.filter(item => item.key !== key)
+            })
+        }
+
+        handleSubmit = (e) => {
+            e.preventDefault();
+            const { validateFields } = this.props.form;
+            validateFields((err, values) => {
+                if (!err) {
+                    console.log(values);
+                    this.props.form.resetFields();
+                    const newData = {
+                        key: this.state.dataCount,
+                        device_id: values.device_id,
+                        location: values.location,
+                        wallet_address: values.wallet_address
+                    }
+                    axios.get('http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/device/add', {
+                        params: {
+                            username: 'admin',
+                            device_id: newData.device_id,
+                            loc: newData.location,
+                            wallet_address: newData.wallet_address
+                        }
+                    }).then(response => {
+                        /* Error */
+                    })
+                    this.setState({
+                        visible: false,
+                        dataSource: [...this.state.dataSource, newData],
+                        dataCount: this.state.dataCount + 1
+                    })
+                }
+            })
+        }
+
+        getRegister() {
+            const { getFieldDecorator } = this.props.form;
+            return (
+                <Modal visible={this.state.visible} footer={null} onCancel={this.cancel}>
+                    <Layout style={{ background: '#fff' }}>
+                        <Header style={{ background: '#fff', padding: 0 }} align="center">
+                            <h1>Register device</h1>
+                        </Header>
+                        <Form>
+                            <Form.Item label="Device id">
+                                {getFieldDecorator('device_id', {
+                                    rules: [{ required: true, message: 'Please input device id!' },]
+                                })(
+                                    <Input />
+                                )}
+                            </Form.Item>
+                            <Form.Item label="Location">
+                                {getFieldDecorator('location', {
+                                    rules: [{ required: true, message: 'Please input location!'},]
+                                })(
+                                    <Input />
+                                )}
+                            </Form.Item>
+                            <Form.Item label="Wallet address">
+                                {getFieldDecorator('wallet_address', {
+                                    rules: [{ required: true, message: 'Please input your wallet address!' },]
+                                })(
+                                    <Input />
+                                )}
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" onClick={this.handleSubmit} htmlType="submit">Register</Button>
+                            </Form.Item>
+                        </Form>
+                    </Layout>
+                </Modal>
+            )
+        }
+        render() {
+            return (
+                <div style={{ padding: '0 0 0 5%' }}>
+                    {this.getRegister()}
+                    <div style={{ padding: '0 0 0 65%' }}>
+                        <Button type="primary" onClick={this.add}> <Icon type="plus" /> Add </Button>
+                    </div>
+                    <Table columns={this.columns} dataSource={this.state.dataSource} style={{ width: '60%' }} pagination={false}/>
                 </div>
-                <Table columns={columns} dataSource={data} style={{width: '60%'}}/>
-            </div>
-        )
-    }
+            )
+        }
 })
 export default Device;
