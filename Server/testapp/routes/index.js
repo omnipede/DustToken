d74945f83d3b1574ea249691bfc3eb5854bb5a37
Lee Dong Seung <tmd3282@u.sogang.ac.kr>
2019-05-26 17:40:22 +0900

    데이터 전송

--- Server/testapp/routes/index.js
+++ Server/testapp/routes/index.js
@@ -36,11 +36,11 @@ connection.connect(function(err){
 
 router.get('/data',function(req,res){
             var date = req.query.date;
-            var temp = req.query.temp;
-            date = moment().add(9,'hour').format('YYYYMMDD,HH:mm:ss')
-            var input = date + ',' + temp+'\n'
+            var pm = req.query.pm;
+            date = moment().add(0,'hour').format('YYYYMMDD,HH:mm:ss')
+            var input = date + ',' + pm+'\n'
             console.log(req.query.date)
-            console.log(req.query.temp) 
+            console.log(req.query.pm) 
             fs.appendFile(file,input,function(err){
                     if(err)throw err
                     console.log('data is written in '+input)
@@ -49,7 +49,7 @@ router.get('/data',function(req,res){
 })
 router.post('/register',function(req,res){
         
-      //  console.log(req.body);
+        console.log(req.body);
         var request = req.body;
         var id = req.body.id;
         var today = new Date();
@@ -137,53 +137,3 @@ router.post('/login',function(req,res){
 
 
 
-// var url = 'http://www.melon.com/chart/';
-// request(url,function(err,response,body){
-//     if(err){
-//         throw err;
-//     }
-//     var rank = 10;
-//     var $  = cheerio.load(body);
-//     var title_info,artist_info;
-//     var title = new Array(),artist = new Array();
-//     var time;
-//     $('.year').each(function(){
-//         time = $(this).text();
-//     })
-//     for(var i=0;i<rank;){
-//         $('.ellipsis.rank01 > span > a').each(function(){
-//             title[i] = title_info = $(this).text();
-//             i++;
-//         })
-//     }
-//     for(var i=0;i<rank;){
-//         $('.ellipsis.rank02>a').each(function(){
-//             artist_info = $(this).text();
-//             artist[i] = artist_info;
-//             i++;
-//         })
-//     }
-
-//     console.log("update :"+time);
-//    for(var i=0;i<rank;i++)
-//     console.log((i+1).toString()+" "+title[i] + '-' + artist[i])
-// })
-
-// var url = "http://cleanair.seoul.go.kr/air_city.htm?method=measure";
-// request(url, function (err, response, body) {
-//     if (err) {
-//         throw err;
-//     }
-
-//     var $ = cheerio.load(body);
-//     console.log(body)
-
-//     var info;
-//     $('.b').each(function () {
-//         info = $(this).text();
-//     })
-
-
-
-//     console.log(info);
-// })
