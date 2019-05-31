7bd98acbd01cf0050a41e9cce065ac15da76d1b5
omnipede <omnipede@naver.com>
2019-05-31 22:14:19 +0900

    미세먼지 Chart 구현
    google line chart 이용해서 미세먼지 농도 변화를 차트로 그림.

--- Client/dustgate/src/Components/API.js
+++ Client/dustgate/src/Components/API.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Table, Tabs} from 'antd';
+import {Chart} from 'react-google-charts';
 import axios from 'axios';
 const moment = require('moment-timezone');
 const { TabPane } = Tabs;
@@ -28,6 +29,18 @@ class API extends React.Component{
         dataSource: []
     }
 
+    getChartData () {
+        let t = [['time', 'pm25', 'pm10']]
+        let src = [...this.state.dataSource]
+        src.reverse();
+        src.forEach((item, index, array) => {
+            let d = [item.time, Number(item.pm25), Number(item.pm10)]
+            //console.log(moment(new Date(item.time)).tz('Asia/Seoul').format('HH:mm'));
+            t.push(d);
+        })
+        return t;
+    }
+
     componentDidMount() {
         axios.get('http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/list')
         .then(response => {
@@ -50,7 +63,43 @@ class API extends React.Component{
                     <div> <Table columns={columns} dataSource={this.state.dataSource} /> </div>
                 </TabPane>
                 <TabPane tab="Chart" key="2">
-                    Content of chart.
+                    <Chart
+                        height={500}
+                        chartType="Line"
+                        loader={
+                            <div> loading ... </div>
+                            
+                        }
+                        data={
+                            this.getChartData()
+                            /*[
+                            [
+                                'time',
+                                'pm2.5',
+                                'pm10',
+                            ],
+                            [1, 37.8, 80.8],
+                            [2, 30.9, 69.5],
+                            [3, 25.4, 57],
+                            [4, 11.7, 18.8],
+                            [5, 11.9, 17.6],
+                            [6, 8.8, 13.6],
+                            [7, 7.6, 12.3],
+                            [8, 12.3, 29.2],
+                            [9, 16.9, 42.9],
+                            [10, 12.8, 30.9],
+                            [11, 5.3, 7.9],
+                            [12, 6.6, 8.4],
+                            [13, 4.8, 6.3],
+                            [14, 4.2, 6.2],
+                        ] */}
+                        options={{
+                            chart: {
+                                title: 'PM2.5 & PM10 change',
+                                subtitle: 'µg /  m³',
+                            },
+                        }}
+                    />
                 </TabPane>
             </Tabs>
             
