0ca70f23a5ce4b3a26cc5f4395b15eacd99af29b
omnipede <omnipede@naver.com>
2019-06-16 16:23:26 +0900

    Data 검색 기능 추가

--- Client/dustgate/src/Components/API.js
+++ Client/dustgate/src/Components/API.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Table, Tabs, Button, Icon, Modal } from 'antd';
+import {Table, Tabs, Button, Icon, Modal, Input } from 'antd';
 import {Chart} from 'react-google-charts';
 import axios from 'axios';
 const moment = require('moment-timezone');
 const { TabPane } = Tabs;
+const Search = Input.Search;
 const columns = [
     { title: 'Location', dataIndex: 'location'}, 
     { title: 'pm25', dataIndex: 'pm25'},
@@ -16,6 +17,7 @@ class API extends React.Component{
     state = {
         dataCount: 0,
         dataSource: [],
+        searchLocation: '',
         visible: false
     }
 
@@ -30,8 +32,12 @@ class API extends React.Component{
         return t;
     }
 
-    componentDidMount() {
-        axios.get('http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/list')
+    fetchData (loc) {
+        axios.get('http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/list', {
+            params: {
+                location: loc
+            }
+        })
         .then(response => {
             let i = this.state.dataCount;
             let r = response.data.map((v) => {
@@ -44,6 +50,10 @@ class API extends React.Component{
                 dataSource: [...r]
             })
         })
+    }
+
+    componentDidMount() {
+        this.fetchData(this.state.searchLocation);
     }
 
     render() {
@@ -81,7 +91,7 @@ class API extends React.Component{
                         http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/get_data?api_key=YOUR_API_KEY&from=1559307591528&to=1559308187629&count=128
                         </a>
                     </div>
-                    <h1> Transaction 데이터 API </h1>
+                    <h1> 블록체인 데이터 API </h1>
                     <div style={{backgroundColor:'#f8f9fa', margin: '0 0 8px', padding: '12px'}} >
                         <a href='http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/get_transaction?api_key=YOUR_API_KEY&from=1559307591528&to=1559308187629&count=128'> 
                         http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/get_transaction?api_key=YOUR_API_KEY&from=1559307591528&to=1559308187629&count=128
@@ -91,6 +101,8 @@ class API extends React.Component{
 
                 <TabPane tab="Data" key="1">
                     <div>
+                        
+                        {/* CSV download */}
                         <Button type='primary' style={{ margin: '0 0 20px 0' }}
                             onClick = {async (e) => {
                                 e.preventDefault();
@@ -100,14 +112,14 @@ class API extends React.Component{
                                     .send({from: accounts[0]}, (err, txHash) => {
                                         if (!err){ 
                                             axios({
-                                                url: 'http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/export_data.csv?count=1000',
+                                                url: `http://ec2-54-186-81-184.us-west-2.compute.amazonaws.com:3001/api/export_data.csv?count=1000&location=${this.state.searchLocation}`,
                                                 method: 'GET',
                                                 responseType: 'blob', // important
                                               }).then((response) => {
                                                  const url = window.URL.createObjectURL(new Blob([response.data]));
                                                  const link = document.createElement('a');
                                                  link.href = url;
-                                                 link.setAttribute('download', 'file.csv'); //or any other extension
+                                                 link.setAttribute('download', this.state.searchLocation + '_data.csv'); //or any other extension
                                                  document.body.appendChild(link);
                                                  link.click();
                                               });
@@ -117,6 +129,18 @@ class API extends React.Component{
                             }}>
                             <Icon type='download' /> Download as csv
                         </Button>
+                        {/*Search area*/}
+                        <span style={{float: 'right'}}>
+                        <Search
+                            placeholder="input location"
+                            enterButton="Search"
+                            onSearch={(value) => {
+                                this.fetchData(value)
+                                this.setState({searchLocation: value})
+                            }}
+                            style={{ width: 400, margin: '0 10px 0 10px'}}
+                        />
+                        </span>
                         <Table columns={columns} dataSource={this.state.dataSource} />
                     </div>
                 </TabPane>
--- Client/dustgate/src/Components/Intro.js
+++ Client/dustgate/src/Components/Intro.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Carousel, Row, Col } from 'antd';
 import "antd/dist/antd.css";
 import img_dust from '../intro_dust.jpg';
-import img_token from '../intro_token.jpg';
 import img_blockchain from '../img_blockchain.jpg';
+import img_sensor from '../intro_sensor.jpg';
 
 class Intro extends React.Component{
     render() {
         return(
             <div>
-                <Carousel>
+                <Carousel autoplay>
                     <div>
                         <Row>
                             <Col span={10}> <img src={img_dust} alt='logo' style={{width: '100%', height: '360px'}}/></Col>
@@ -18,7 +18,7 @@ class Intro extends React.Component{
                     </div>
                     <div>
                         <Row>
-                            <Col span={10}> <img src={img_token} alt='logo' style={{width: '100%', height: '360px'}}/></Col>
+                            <Col span={10}> <img src={img_sensor} alt='logo' style={{width: '80%', height: '360px'}}/></Col>
                             <Col span={14} style={{color: 'white', fontSize: 36, textAlign: 'center'}}> 국내유일 IoT 기반 암호화폐 </Col>
                         </Row>
                     </div>
@@ -34,21 +34,6 @@ class Intro extends React.Component{
                     <div style={{backgroundColor: '#555555', margin: 'auto', height: '2px', width: '70px', boxSizing: 'border-box' }}> </div>
                     <div style={{color: '#555555', fontSize: 13, textAlign: 'center', margin: '10px 0px 30px'}}>IoT based Ethereum ERC20 Token</div>
                 </div>
-                {/*
-                <div style={{margin: '50px 0 0 0'}}>
-                    <h2>Getting started</h2>
-                <Collapse>
-                    <Panel header="Step 1: 디바이스 구입" key="1">
-                    </Panel>
-                    <Panel header="Step 2: 디바이스 등록" key="2">
-                        <p></p>
-                    </Panel>
-                    <Panel header="Step 3: 편히 쉬세요" key="3">
-                        <p></p>
-                    </Panel>
-                </Collapse>
-                </div>
-            */}
             </div>
         );
     }
